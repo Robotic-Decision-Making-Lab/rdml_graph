f51678a52870d946f0e6735c5c3fa80a526df92d
Ian Rankin <ianrancon@gmail.com>
2020-02-22 17:39:38 -0800

    Added main framework for MCTS and random rollout function

--- rdml_graph/MCTS.py
+++ rdml_graph/MCTS.py
@@ -7,19 +7,95 @@
 
 
 import tqdm
+import numpy as np
+from rdml_graph import MCTSTree
 
 
 
 # MCTS
 # The main entry function to the MCTS algorithm.
-def MCTS(start, max_iterations, data=None):
+# @param start - the entry state of the MCTS algorithm
+# @param max_iterations - maximum number of iterations the MCTS algorithm runs.
+# @oaram budget - the total budget of
+def MCTS(   start, max_iterations, rewardFunc, budget=1.0, selection=UCBSelection, \
+            rolloutFunc=randomRollout, solutionFunc=bestReward, data=None):
+    # Set the root of the search tree.
+    root = MCTSTree(start, 0, None)
+    root.unpicked_children = root.successor()
 
+    bestReward = -np.inf
+    bestLeaf = None
 
-
-    # main loop of mcts
+    # Main loop of MCTS
     for i in tqdm.tqdm(range(max_iterations)):
         current = root
 
-        # SELECTION AND EXPANSION
+        ######### SELECTION and Expansion
+        # Check all possibilties of selection.
+        while True:
+            if len(current.unpicked_children) > 0:
+                ######## Expansion
+                child = current.expandNode(budget)
+                child.unpicked_children = child.successor()
 
-        pass
+                current = child
+
+                # once a node has been successfully expanded break out of selection loop.
+                break
+            else:
+                ######## Selection.
+                current = selection(current, budget, data)
+
+                break
+        # end selection expansion while loop.
+
+        ######## ROLLOUT
+        # perform rollout to the end of a possible sequence.
+        leaf = rolloutFunc(current, budget, data)
+        rolloutReward = rewardFunc(leaf.getPath(), budget, data)
+
+        # Keep track of best reward
+        if rolloutReward > bestReward
+
+        ######## BACK-PROPOGATE
+        leaf.backpropReward()
+
+    # end main for loop
+
+    ######## SOLUTION
+    solutionLeaf = solutionFunc(root, bestLeaf, bestReward, data)
+    return solutionLeaf.getPath()
+# End MCTS
+
+########################### Common functions for MCTS
+
+############## rollout functions
+
+# randomRollout
+# This function performs rollout using random child selection.
+# @param treeState - the current state of the rollout function
+# @param budget - the budget of the sequence
+# @param data - a generic structure to store data for a rollout.
+def randomRollout(treeState, budget, data=None):
+    succ = treeState.successor()
+    if len(succ) == 0:
+        return self
+
+    childIdx = np.random.randint(0, len(succ))
+
+    if succ[childIdx].cost() > budget:
+        return self
+    else:
+        self.children.append(succ[childIdx])
+        return randomRollout(succ[childIdx])
+
+############### solution functions
+
+# bestReward
+# This function performs rollout using random child selection.
+# @param root - the current state of the rollout function
+# @param bestLeaf - the best possible leaf
+# @param bestR - the best seen reward
+# @param data - generic data possibly useful for the best reward.
+def bestReward(root, bestLeaf, bestR, data=None):
+    return bestLeaf
--- rdml_graph/MCTSTree.py
+++ rdml_graph/MCTSTree.py
@@ -8,11 +8,57 @@
 from rdml_graph import State
 from rdml_graph import SearchState
 
+import numpy as np
+
 class MCTSTree(SearchState):
     # Constructor
 
     def __init__(self, state, rCost, parent, ):
-        super(MCTSTree, self).__init__(state, rCost=rCost, hCost=0 parent=parent)
+        super(MCTSTree, self).__init__(state, rCost=rCost, hCost=0, parent=parent)
 
-        self.unpicked_children = 
+        self.unpicked_children = []
+        self.children = []
+        self.sum_reward = 0
         self.num_updates = 0
+
+    # reward
+    # reward is an average of all total rewards propagated through the tree.
+    # @return - reward estimate of current state.
+    def reward(self):
+        return self.sum_reward / self.num_updates
+
+    def backpropReward(self, reward):
+        self.sum_reward  += reward
+        self.num_updates += 1
+
+        if self.parent is not None:
+            self.parent.backpropReward(reward)
+
+    # expandNode
+    # expands the current node at the given index.
+    # If idx is none, then a random unpicked child is selected.
+    # @param idx - the index of the unpicked child to expand (normaly random)
+    #
+    # @return - the MCTSTree to expand
+    # @post - unpicked children has child removed, added to children
+    def expandNode(self, idx = None):
+        childIdx = np.random.randint(0, len(self.unpicked_children))
+        child = self.unpicked_children[childIdx]
+        del self.unpicked_children[childIdx]
+        self.children.append(child)
+
+        return child
+
+
+    # successor
+    # successor function for states that removes children over budget.
+    # @param budget - the max budget of the successor function.
+    #
+    # @return - list of MCTSTree objects.
+    def successor(self, budget=np.inf):
+        result = []
+        for s in self.state.succesor():
+            cost = self.rCost + s[1]
+            if cost <= budget:
+                result.append(MCTSTree(s[0], cost, self))
+        return result
