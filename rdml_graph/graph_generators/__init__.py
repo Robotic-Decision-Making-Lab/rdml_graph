9a6ed8c62f29e3fbfb373b7fb5647443d92069e6
Ian Rankin <rankini@oregonstate.edu>
2020-02-26 16:32:52 -0800

    Changed graph plot to fit library

--- rdml_graph/graph_generators/__init__.py
+++ rdml_graph/graph_generators/__init__.py
@@ -1,1 +1,1 @@
-#from .GraphSearch import AStar, graph_goal_check, partial_homotopy_goal_check
+from .PRM import PRM
--- rdml_graph/plot/GraphPlot.py
+++ rdml_graph/plot/GraphPlot.py
@@ -8,32 +8,16 @@ import Path
 import numpy as np
 import matplotlib.pyplot as plt
 
-# plotPath
-# Plots a geometric path on a graph structure.
-def plotPath(path, color='blue'):
-    if path != None:
-        x = np.empty(len(path.states))
-        y = np.empty(len(path.states))
-        i = 0
-        for s in path.states:
-            #print(str(s.id) + ' - ')
-            x[i] = s.pt[0]
-            y[i] = s.pt[1]
-            i += 1
-
-        plt.plot(x,y, color=color)
-    else:
-        print('Path does not exist')
 
 # plot2DGeoGraph
 # This assumes that the graph is given a geometric graph
 # with 2d points, if it is not, this function may fail.
 # This function is not effcient
-# @param G - the input graph
+# @param G - list of all nodes in graph
 # @param color - the color of edges
 def plot2DGeoGraph(G, color='blue'):
-    for id in G:
-        plotEdgesFromNode(G[id], color)
+    for n in G:
+        plotEdgesFromNode(n, color)
 
 
 def plotEdgesFromNode(n, color='blue'):
