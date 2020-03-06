6aaed02246dd0b0c7b1a4bd63defecbc88040812
Ian Rankin <ianrancon@gmail.com>
2020-03-05 17:15:40 -0800

    Fixed grammar error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(name='rdml_graph',
       version='0.0.2',
-      description='Package for Robot Decision Making Lab various graph functions Ian developed',
+      description='Package for Robot Decision Making Lab with various graph functions Ian developed',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='http://tb.d',
@@ -14,5 +14,6 @@ setup(name='rdml_graph',
       license='None',
       packages=['rdml_graph'],
       install_requires=['numpy>=1.3.0','matplotlib>=2.0.0', 'scipy>=1.0.0', 'tqdm>=3.0.0'],
+      extras_require={'Saving graphs': ["pickle"]},
       python_requires='>=2.7',
       zip_safe=False)
