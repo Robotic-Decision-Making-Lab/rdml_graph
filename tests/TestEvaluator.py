269282177474b20ab070dacbe88ab903239a2a3f
Ian <rankini@oregonstate.edu>
2021-07-23 14:12:51 -0700

    Updated test of evaluator because it had a bug and was not performing a good test

--- tests/TestEvaluator.py
+++ tests/TestEvaluator.py
@@ -7,14 +7,14 @@ import numpy as np
 import rdml_graph as gr
 
 x_axis = 20
-y_axis = 15
+y_axis = 25
 
 info_field = np.ones((x_axis,y_axis, 2)) * np.array([2,3])
 eval = gr.MaskedEvaluator(info_field, \
-                    np.arange(0,x_axis), np.arange(0,y_axis), \
+                    np.arange(-x_axis,x_axis,2), np.arange(0,y_axis), \
                     radius=1.5)
 
-path = np.array([[3.1,4.2], [8, 5], [13,12], [21,13]])
+path = np.array([[3.1,4.2], [8, 5], [13,12], [-10,13]])
 
 score = eval.getScore(path)
 
