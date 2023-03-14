71e298338afa258bc93decb3f4328dcbd31c2166
ianran <rankini@oregonstate.edu>
2023-03-13 17:54:37 -0700

    First commit of mutual information learner

---
+++ src/rdml_graph/gaussian_process/MutualInformationLearner.py
@@ -0,0 +1,29 @@
+# MutualInformationLearner.py
+# Written Ian Rankin - March 2023
+#
+# A set of code to select active learning algorithms for user preferences.
+# This code implements the mutual information model described in 
+# Asking Easy Questions: A User-Friendly Approach to Active Reward Learning (2019) 
+#    E. Biyik, M. Palan, N.C. Landolfi, D.P. Losey, D. Sadigh
+#
+# Potentially with modifications
+
+
+
+from rdml_graph.gaussian_process import ActiveLearner
+import numpy as np
+
+
+class MutualInformationLearner(ActiveLearner):
+
+    ## constructor for the mutual information learner.
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
